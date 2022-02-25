095a3df341305ca97fea5107283ab4e448f019a0
Loïc Conan <lconan@enssat.fr>
2022-02-25 08:40:06 +0100

    MàJ tp4

--- js/content/script/JS_TP4.js
+++ js/content/script/JS_TP4.js
@@ -3,6 +3,9 @@ var worker = new Worker('./content/script/TP4_worker.js');
 const canvas = document.getElementById("cvs");
 const ctx = canvas.getContext('2d');
 const interval = setInterval(creerPoint, 100);
+const maxRequest = 100;
+
+let nbRequest = 0;
 
 ctx.fillStyle = "red";
 
@@ -11,5 +14,14 @@ function creerPoint() {
 }
 
 worker.onmessage = function(e) {
-    console.log(e.data);
+    if (nbRequest < maxRequest) {
+        console.log(e.data);
+        ctx.beginPath();
+        ctx.arc(xGET,yGET, 10, 0, 360);
+        ctx.stroke();
+        nbRequest++;
+    }
+    else {
+        clearInterval(interval);
+    }
 }
